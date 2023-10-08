15dc6f43d369c2abe16d90354f46287665f1c8bb
Meteorych <super.titlov@inbox.ru>
2023-10-08 23:25:25 +0300

    fixes

--- lab5AIPOS/main.py
+++ lab5AIPOS/main.py
@@ -3,6 +3,7 @@ import json
 import server
 from client import send_request
 
+payload = {'key1': 'value1', 'key2': 'value2'}
 def main():
     parser = argparse.ArgumentParser(description='HTTP Server and Client')
     parser.add_argument('--port', type=int, default=8000, help='Port to listen on for the server')
@@ -10,7 +11,7 @@ def main():
     parser.add_argument('--url', type=str, help='URL to send the request to')
     parser.add_argument('--method', type=str, default='GET', help='HTTP method (GET by default), also there is support for POST and OPTIONS')
     parser.add_argument('--headers', type=str, help='Headers as a JSON string')
-    parser.add_argument('--data', type=str, help='Request data')
+    parser.add_argument('--data', type=str, help='Request data', default=payload)
     args = parser.parse_args()
 
     if args.start_server:
--- lab5AIPOS/server.py
+++ lab5AIPOS/server.py
@@ -1,4 +1,4 @@
-import argparse
+import urllib.parse
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class OurHttpHandler(BaseHTTPRequestHandler):
@@ -7,21 +7,32 @@ class OurHttpHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write(b'Get request!')
+        self.wfile.write(b'Hello world!')
 
     def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length).decode('utf-8')
+
+        # Parse the POST data as needed (e.g., as URL-encoded or JSON data)
+        # For example, if it's URL-encoded data:
+        post_data_dict = urllib.parse.parse_qs(post_data)
+
+        # Access and process the data from post_data_dict
+        if 'key' in post_data_dict:
+            value = post_data_dict['key'][0]
+            print(f'Received POST data: {value}')
+
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write(b'Post request')
-        pass
+        self.wfile.write(b'POST request is received and processed')
 
     def do_OPTIONS(self):
+        # Handle OPTIONS request
         self.send_response(200)
-        self.send_header('Content-type', 'text/html')
+        self.send_header('Allow', 'GET, POST, OPTIONS')
         self.end_headers()
-        self.wfile.write(b'Options request')
-        pass
+        self.wfile.write(b'Allowed methods: GET, POST, OPTIONS')
 
 def run_server(port):
     server_address = ('', port)
